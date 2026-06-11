2819a09573bfcd0fb94135c3dcad61993de82621
batmanvigilante7 <hemanthsairoyal7@gmail.com>
2026-06-11 23:59:19 +0530

    Add cohesive CompactMacbookStage in compact mode inside MacbookScroll

--- src/components/ui/macbook-scroll.tsx
+++ src/components/ui/macbook-scroll.tsx
@@ -85,6 +85,100 @@ export const MacbookScroll = ({
     [1, 0]
   );
 
+  if (compact) {
+    return (
+      <div
+        ref={ref}
+        className="flex shrink-0 scale-[0.35] transform flex-col items-center py-0 [perspective:800px] sm:scale-50 md:scale-100 min-h-[76vh] md:py-0 justify-center"
+      >
+        <motion.div
+          initial={{ opacity: 0, y: 40, scale: 0.94 }}
+          whileInView={{ opacity: 1, y: 0, scale: 1 }}
+          viewport={{ once: true, amount: 0.35 }}
+          transition={{ duration: 0.7, ease: [0.16, 1, 0.3, 1] }}
+          className="relative flex flex-col items-center"
+        >
+          {/* Lid in fixed open state */}
+          <div className="relative [perspective:800px]">
+            {/* Lid back */}
+            <div
+              style={{
+                transform: "perspective(800px) rotateX(-25deg) translateZ(0px)",
+                transformOrigin: "bottom",
+                transformStyle: "preserve-3d",
+              }}
+              className="relative h-[12rem] w-[32rem] rounded-2xl bg-[#010101] p-2"
+            >
+              <div
+                style={{
+                  boxShadow: "0px 2px 0px 2px #171717 inset",
+                }}
+                className="absolute inset-0 flex items-center justify-center rounded-lg bg-[#010101]"
+              >
+                <span className="text-white">
+                  <AceternityLogo />
+                </span>
+              </div>
+            </div>
+            
+            {/* Screen Lid (Opened) */}
+            <div
+              style={{
+                scaleX: isMobile ? 1 : 1.35,
+                scaleY: isMobile ? 1 : 1.35,
+                rotateX: 0,
+                translateY: 0,
+                transformStyle: "preserve-3d",
+                transformOrigin: "top",
+              }}
+              className="absolute inset-0 h-96 w-[32rem] rounded-2xl bg-[#010101] p-2"
+            >
+              <div className="absolute inset-0 rounded-lg bg-[#272729]" />
+              {children ? (
+                <div className="absolute inset-2 rounded-lg bg-[#050505] overflow-auto z-10 flex flex-col text-left pointer-events-auto">
+                  {children}
+                </div>
+              ) : (
+                src && (
+                  <img
+                    src={src}
+                    alt="screen content"
+                    className="absolute inset-0 h-full w-full rounded-lg object-cover object-left-top"
+                  />
+                )
+              )}
+            </div>
+          </div>
+          
+          {/* Base Area (Cohesive Keyboard base) */}
+          <div className="relative -z-10 h-[22rem] w-[32rem] overflow-hidden rounded-2xl bg-gray-200 dark:bg-[#272729]">
+            {/* above keyboard bar */}
+            <div className="relative h-10 w-full">
+              <div className="absolute inset-x-0 mx-auto h-4 w-[80%] bg-[#050505]" />
+            </div>
+            <div className="relative flex">
+              <div className="mx-auto h-full w-[10%] overflow-hidden">
+                <SpeakerGrid />
+              </div>
+              <div className="mx-auto h-full w-[80%]">
+                <Keypad />
+              </div>
+              <div className="mx-auto h-full w-[10%] overflow-hidden">
+                <SpeakerGrid />
+              </div>
+            </div>
+            <Trackpad />
+            <div className="absolute inset-x-0 bottom-0 mx-auto h-2 w-20 rounded-tl-3xl rounded-tr-3xl bg-gradient-to-t from-[#272729] to-[#050505]" />
+            {showGradient && (
+              <div className="absolute inset-x-0 bottom-0 z-50 h-40 w-full bg-gradient-to-t from-white via-white to-transparent dark:from-black dark:via-black"></div>
+            )}
+            {badge && <div className="absolute bottom-4 left-4">{badge}</div>}
+          </div>
+        </motion.div>
+      </div>
+    );
+  }
+
   return (
     <div
       ref={ref}
